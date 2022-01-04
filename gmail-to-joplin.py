d0132a6c2f3414a205cd4deb4d89323dab838c05
Torbjørn Kveberg <tkvebbs@gmail.com>
2022-01-04 01:29:22 +0100

    Improved user friendliness, error handling and logging

--- gmail-to-joplin.py
+++ gmail-to-joplin.py
@@ -6,17 +6,16 @@ Retrieves unread e-mails (from, subject, main text and attachments) from a Gmail
  - Joplin CLI https://joplinapp.org/terminal/
  - Gmail account with API enabled https://console.cloud.google.com/home/
  - OAuth client credentials from said Gmail account stored as credentials.json the same folder as this script
- - A target notebook in Joplin CLI with the same name as in the "JOPLIN_INBOX" variable in the CONFIG-section below.
- 
- Consider adding pre-approved addresses to the "APPROVED_SENDERS" variable in the CONFIG-section below, to avoid importing unwanted e-mails.
 """
-
 # IMPORTS
 import os.path
+import subprocess
 import shutil
-from datetime import datetime
 import base64
 import email
+import logging
+from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -24,27 +23,15 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
 # CONFIG:
-# Target Joplin notebook
-JOPLIN_INBOX = "_inbox"
-# List of addresses you want to import mails from (leave blank if you want everything)
-APPROVED_SENDERS = []
-# A log-file (log.txt) is created in the script directory. You can specify the number of entries it should hold here.
-LOG_SIZE = 100
-# A download folder to temporarily hold e-mails and attachments
-DOWNLOAD_PATH = f"{os.getcwd()}\\gmtj_downloads"
-# Debug boolean. If True, Joplin will not be called to sync and unread mails will not be moved to trash.
-DEBUG = False
+JOPLIN_INBOX = "_inbox"                              # Target Joplin notebook. Script creates it if it does not already exist
+APPROVED_SENDERS = []                                # List of addresses you want to import mails from. Leave blank if you want everything
+LOG_SIZE = 5000                                      # Max size of log file in bytes
+DOWNLOAD_PATH = f"{os.getcwd()}\\gmtj_downloads"     # A download folder to temporarily hold e-mails and attachments
 
-
-# PREPARE FOR DOWNLOADS
-if os.path.exists(DOWNLOAD_PATH):
-    shutil.rmtree(DOWNLOAD_PATH)
-    os.mkdir(DOWNLOAD_PATH)
-else:
-    os.mkdir(DOWNLOAD_PATH)
-
+DEBUG = False                                        # If True:  1) Joplin will not be called to sync 
+#                                                                2) Unread mails will not be moved to trash.
+#                                                                3) JOPLIN_INBOX is set to _debug         
 
 """
 TALK TO JOPLIN
@@ -53,29 +40,40 @@ TALK TO JOPLIN
 def import_to_joplin(id, subject, text, attachments):
     """ Imports text and attachments to Joplin-notebook, with subject as title """
     
-    # Store text in file using its unique id
-    path = f"gmtj_downloads\\{id}\\"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    
-    text = text.encode()
-    text_file = str(path + id + ".md")
-    with open(text_file, "wb") as f:
-        f.write(text)
+    try: 
+        # Store text in file using its unique id
+        path = f"gmtj_downloads\\{id}\\"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        text = text.encode()
+        text_file = str(path + id + ".md")
+        with open(text_file, "wb") as f:
+            f.write(text)
 
-    # Import text as new note in joplin_inbox
-    path_to_text = os.getcwd() + "\\" + text_file
-    os.system("joplin import \"" + path_to_text + "\" " + JOPLIN_INBOX)
-    
-    # Append attachments using list of attachment paths
-    if attachments:
-        for i in attachments:
-            os.system("joplin attach " + "\"" + id + "\" \"" + i + "\"")
-    
-    # Use GMail subject to set title
-    os.system("joplin set \"" + id +"\"" + " title \"" + subject + "\"")
+        # Import text as new note in joplin_inbox. Create inbox if it does not exist.
+        path_to_text = os.getcwd() + "\\" + text_file
+        result = subprocess.run(f"joplin import \"{path_to_text}\" {JOPLIN_INBOX}", shell=True, capture_output=True)
+        
+        if str(result.stdout.decode()) == f"Cannot find \"{JOPLIN_INBOX}\".\n":
+            logger.warning(f"Had to create target notebook \"{JOPLIN_INBOX}\". Restart of Joplin CLI may be required.")
+            subprocess.run(f"joplin mkbook {JOPLIN_INBOX}", shell=True)
+            subprocess.run(f"joplin import \"{path_to_text}\" {JOPLIN_INBOX}", shell=True)
+        
+        # Append attachments using list of attachment paths
+        if attachments:
+            for i in attachments:
+                subprocess.run(f"joplin attach \"{id}\" \"{i}\"", shell=True)
+        
+        # Use GMail subject to set title
+        subprocess.run(f"joplin set \"{id}\" title \"{subject}\"", shell=True)
 
-    return
+        return True
+    
+    except Exception as e:
+        logger.error(f"Something went wrong during import of {id} on {subject}. {e}.")
+
+        return False
 
 
 """ 
@@ -105,30 +103,31 @@ if not creds or not creds.valid:
 
 def check_gmail():
     """ Returns list of Gmail ids for any unread mail. """
+
     try:
         service = build("gmail", "v1", credentials=creds)
-        
         result = service.users().messages().list(userId="me", q="is:unread").execute()
         
         # Print number of e-mails
         n_new_mails = result["resultSizeEstimate"]
-        print(str(n_new_mails) + " unread mail(s) found!")
+        print(f"{n_new_mails} unread mail(s)!")
         if n_new_mails == 0:
-            quit()
-        
+            return []
+
         # Get id's
         messages = result["messages"]
         ids = []
         for i in messages:
             ids.append(i["id"])
         
+        logger.info(f"Found {len(ids)} unread mail(s)")
+
         # Return id's
         return ids
     
     except Exception as e:
-        write_to_log(type="ERROR", entry=f"Unknown exception when calling check_gmail(). {e}")
-        quit()
-
+        logger.error(f"Failed to retrieve Gmail. {e}")
+        
 
 def import_gmail(id):
     """ Downloads Gmail, and passes and any attachments to import_to_joplin() """
@@ -156,9 +155,9 @@ def import_gmail(id):
     if APPROVED_SENDERS:
         if sender not in APPROVED_SENDERS:
             if not DEBUG:
-                service.users().messages().trash(userId="me", id=str(id)).execute()
-            write_to_log(type="ERROR", entry=f"{sender} not in list of approved senders. E-mail {id} put in trash.")
-            print(f"{sender} not in list of approved senders. E-mail {id} put in trash.")
+                service.users().messages().trash(userId="me", id=str(id)).execute()       
+            
+            logger.warning(f"{sender} not in list of approved senders. E-mail {id} put in trash.")
             
             return False
 
@@ -168,12 +167,13 @@ def import_gmail(id):
             if part.get_content_type()=="text/plain":
                 text = part.get_payload(decode=True)
                 text = text.decode()
+        
         except Exception as e:
-            text_decode_error = f" #Decode-Error!\n\"{e}\"\nThings may not look exactly right."
+            text_decode_error = f" # Decode-Error!\n\"{e}\".\n"
             problem_text = part.get_payload(decode=True)
             text = text_decode_error + problem_text.decode("utf-8", "replace")
-            write_to_log(type="ERROR", entry=f"DECODE ERROR {id}\t FROM  {sender}m \t\t\t {subject}. {e}")
-            
+
+            logger.error(f"Trouble decoding {id} from {sender} on {subject}. {e}")
 
     # Get attachments
     att_filenames = []
@@ -194,76 +194,74 @@ def import_gmail(id):
         with open(file_path, "wb") as f:
                 f.write(data)
 
-    print(f"Passing to Joplin: {id} with {len(att_filenames)} attachment(s) from {sender}.")
-
     # Import to Joplin
-    import_to_joplin(id, subject, text, att_filenames)
+    result = import_to_joplin(id, subject, text, att_filenames)
     
-    # Thrash Gmail
+    # Thrash Gmail on successful import
     if not DEBUG:
-        service.users().messages().trash(userId="me", id=str(id)).execute()
+        if result:
+            service.users().messages().trash(userId="me", id=str(id)).execute()
 
-    # Log
-    write_to_log(type="IMPORT", entry=f"{id} with {len(att_filenames)} attachments.\tFROM {sender}.\t\tSUBJECT \"{subject}\".")
+    if result:
+        logger.info(f"IMPORTED: {id} with {len(att_filenames)} attachments.\tFROM: {sender}.\t\tSUBJECT: \"{subject}\".")
+        
+        return True
 
-    return True
+    elif not result:
+        logger.info(f"{id} was NOT imported, and remains unread in Gmail inbox.")
+        
+        return False
 
-"""
-WRITE LOG
-"""
-def write_to_log(type, entry):
-    """ Writes entry to log.txt, and deletes old entries """
-    timestamp = datetime.now()
-    timestamp = timestamp.strftime("%d-%m-%Y %H:%M:%S")
 
-    if not os.path.exists("log.txt"):
-        with open("log.txt", "x") as f:
-            pass
-
-    with open("log.txt", "r") as f:
-        data = f.read().splitlines(True)
-        if len(data) >= LOG_SIZE:
-            with open("log.txt", "w") as f:
-                f.writelines(data[len(data)-LOG_SIZE+1:])
-    
-    log_text = f"{type}\t{timestamp}\t{entry}\n"
-    with open("log.txt", "a") as f:
-        f.write(log_text)
-    
-    return
-  
-   
 """
 RUN THE SCRIPT
 """
-# Lower case to avoid false negatives when get_gmail() checks for them
+logging.basicConfig(
+    handlers=[RotatingFileHandler("log.txt", maxBytes=LOG_SIZE, backupCount=0)],
+    format="%(asctime)s - %(levelname)s -\t %(message)s", 
+    datefmt="%m/%d/%Y %I:%M:%S %p")
+
+logger = logging.getLogger("gmail-to-joplin")
+logger.setLevel(logging.INFO)
+
+if not DEBUG:
+    logger.info("Script is running ...")
+ 
+if DEBUG:
+    JOPLIN_INBOX = "_debug"
+    logger.info("Script is running in DEBUG MODE ...")
+
+# Check for credentials
+if not os.path.exists("credentials.json"):
+    logger.Error("Could not find OATH file \"credentials.json\" in script path.")
+    quit()
+
+# Prepare download path
+if os.path.exists(DOWNLOAD_PATH):
+    shutil.rmtree(DOWNLOAD_PATH)
+    os.mkdir(DOWNLOAD_PATH)
+else:
+    os.mkdir(DOWNLOAD_PATH)
+
+# Avoid false negatives when get_gmail() checks for approved senders
 for i in range(len(APPROVED_SENDERS)):
     APPROVED_SENDERS[i] = APPROVED_SENDERS[i].lower()
-
-# "gmtj_downloads"-folder to temporarily hold data should not exist, but things can happen
-
-if not os.path.exists("credentials.json"):
-    print("Could not find OAUTH file \"credenials.json\".")
-    quit()
 
 # Check for new e-mails
 new_gmails = check_gmail()
 
 # Import e-mails, if any
 counter = 0
-
 if new_gmails:
     for i in new_gmails:
         get_it = import_gmail(i)
         if get_it:
             counter +=1
-            
-print(f"Imported {counter} of {len(new_gmails)} unread mails.")
 
 # Joplin sync
 if new_gmails and not DEBUG:
-    os.system("joplin sync")
+    subprocess.run("joplin sync", shell=True)
 
-# Clean up download folder
-print("Cleaning temporary download folder ...")
+# Clean up and final log entry
 shutil.rmtree(DOWNLOAD_PATH)
+logger.info(f"Script finished. Imported {counter} of {len(new_gmails)} unread mails.\n")
